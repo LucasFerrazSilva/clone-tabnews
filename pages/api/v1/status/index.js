9c7fe5aeaa1fddda142cee4968a1b03f61547c10
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-04-06 00:43:22 +0000

    finish `/api/v1/status` endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result.rows);
-  response.status(200).json({ message: "São acima da média" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("show server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const maxConectionsResult = await database.query("show max_connections;");
+  const maxConectionsValue = parseInt(
+    maxConectionsResult.rows[0].max_connections,
+  );
+
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsQuery = `SELECT count(*)::int FROM pg_stat_activity where datname = $1;`;
+  const openedConnectionsParams = [databaseName];
+  const openedConnectionsResult = await database.query({
+    text: openedConnectionsQuery,
+    values: openedConnectionsParams,
+  });
+  const openedConnectionsValue = openedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: maxConectionsValue,
+        used_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
