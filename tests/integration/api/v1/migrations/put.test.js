0b2a58220c77897ae05280244604398d7451d624
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-09-15 11:55:45 +0000

    test: rename all descriptions to use new pattern

--- tests/integration/api/v1/migrations/put.test.js
+++ tests/integration/api/v1/migrations/put.test.js
@@ -6,12 +6,16 @@ beforeAll(async () => {
   await database.query("drop schema public cascade; create schema public;");
 });
 
-test("PUT to /api/v1/status should return 405", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/migrations", {
-    method: "PUT",
-  });
-  const responseBody = await response.text();
+describe("PUT to /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("Getting an error response", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/migrations", {
+        method: "PUT",
+      });
+      const responseBody = await response.text();
 
-  expect(response.status).toBe(405);
-  expect(responseBody).toBe("");
+      expect(response.status).toBe(405);
+      expect(responseBody).toBe("");
+    });
+  });
 });
